4acee42b34569b5ce22a76f279da043be9ff8303
Jeviikki <jeviik24@gmail.com>
2026-08-21 11:50:57 +0300

    replaced a for loop with for...of, deleted an unused variable, made let variables into consts

--- Osio 2/teht6/teht6.js
+++ Osio 2/teht6/teht6.js
@@ -1,5 +1,5 @@
 function submitMovie(title, rating) {
-    let movie = {
+    const movie = {
         title : title,
         rating : rating,
     }
@@ -8,28 +8,27 @@ function submitMovie(title, rating) {
 }
 
 function sortMovies(array) {
-    let sorted = array.toSorted((b, a) => (
+    const sorted = array.toSorted((b, a) => (
         a.rating > b.rating ? 1 : b.rating > a.rating ? -1 : 0));
     return sorted
     
 }
 
 let movies = []
-let favStars
 
 /* get data for movies */
 
 const times = prompt("How many movies would you like to rate?");
 
 for (n = 1; n<= times; n++) {
-    let title = prompt("Enter title for movie number "+ n +":");
-    let rating = prompt("How would you rate "+ title +" from 1 to 5?");
+    const title = prompt("Enter title for movie number "+ n +":");
+    const rating = prompt("How would you rate "+ title +" from 1 to 5?");
     submitMovie(title, rating)
 }
 
 /* sort movies */
 
-let sortedMovies = sortMovies(movies);
+const sortedMovies = sortMovies(movies);
 
 /* display on HTML */
 
@@ -38,8 +37,8 @@ document.getElementById("favorite-movie-rating").innerHTML = sortedMovies[0].rat
 
 list = document.getElementById("movies-list");
 
-for (n = 0; n<= sortedMovies.length-1; n++) {
-    let li = document.createElement("li");
-    li.innerHTML = "<b>" + sortedMovies[n].title + "</b> " + sortedMovies[n].rating + "</b> stars"
+for (let item of sortedMovies) {
+    const li = document.createElement("li");
+    li.innerHTML = "<b>" + item.title + "</b> " + item.rating + "</b> stars"
     list.appendChild(li);
 }
