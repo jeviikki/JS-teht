dd44f859736fee42c38769f98c27b82f0ea5883e
Jeviikki <jeviik24@gmail.com>
2026-08-24 10:26:42 +0300

    completed exercise 2

--- Osio_3/t2/t2.js
+++ Osio_3/t2/t2.js
@@ -28,3 +28,26 @@ const todoList = [
 ];
 
 // add your code here
+ul = document.getElementsByTagName("ul")[0];
+
+for (const item of todoList) {
+
+	const li = document.createElement("li");
+
+	const input = document.createElement("input");
+	input.setAttribute("type", "checkbox");
+	input.setAttribute("id", `todo-${item.id}`);
+
+	if (item.completed == true) {
+		input.setAttribute("checked", "");
+	}
+
+	const label = document.createElement("label");
+	label.htmlFor = `todo-${item.id}`;
+	label.innerText = item.task;
+
+	li.appendChild(input);
+	li.appendChild(label);
+
+	ul.appendChild(li);
+}
