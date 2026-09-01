160eb766ada52d44918e824817b3b812bb68152f
Jeviikki <jeviik24@gmail.com>
2026-09-01 14:41:46 +0300

    enhanced some parts of the restaurant app

---
+++ Advanced1/App/components.js
@@ -0,0 +1,40 @@
+const restaurantRow = (restaurant) => {
+    const {name, address = "Unknown"} = restaurant;
+	const tr = document.createElement("tr");
+    tr.innerHTML = `<td>${name}</td><td>${address}</td>`;
+    return tr;
+}
+
+const restaurantModal = (restaurant, menu) => {
+	const {name, address, postalCode, city, phone, company} = restaurant;
+	console.log(menu)
+
+	let menuHtml
+
+	if (menu.length < 1) {
+		menuHtml = `Empty.`;
+	} else {
+		menuHtml = "<ul>";
+		for (const course of menu) {
+			let {name, price, diets} = course;
+			price = price || "? €";
+			menuHtml += `<li><b>${name}</b> (${diets}) ${price}</li>`;
+		}
+		menuHtml += "</ul>"
+	}
+
+	const dialogHtml = `<h2>Restaurant info</h2>
+		<span id="place-name"><b>Restaurant name:</b> ${name}</span>
+		<span id="place-address"><br><b>Address:</b> ${address}, ${postalCode} ${city}</span>
+		<span id="place-phone"><br><b>Phone number:</b> ${phone}</span>
+		<span id="place-company"><br><b>Company:</b> ${company}</p></span>
+		<h2>Today's menu</h2>
+		${menuHtml}`
+
+	return dialogHtml;
+}
+
+export{
+	restaurantModal,
+	restaurantRow
+}; 
---
+++ Advanced1/App/main.js
@@ -0,0 +1,62 @@
+import { restaurantModal, restaurantRow } from "./components.js";
+
+"use strict";
+
+async function getRestaurants() {
+	const target = document.querySelector("table");
+	try {
+		// gets restaurants
+		const response = await fetch("https://media1.edu.metropolia.fi/restaurant/api/v1/restaurants");
+		if (!response.ok) throw new Error("invalid");
+		const restaurants = await response.json();
+
+		//sorts restaurants
+		restaurants.sort((a,b) => a.name > b.name);
+
+		for (const restaurant of restaurants) {
+
+			// adds restaurants
+			const row = restaurantRow(restaurant);
+			row.addEventListener("click", function(evt){
+
+				// highlight
+				for (const row of allRows) {
+					row.classList.remove("highlight");
+				}
+
+				row.classList = "highlight";
+
+				getMenu(restaurant, `https://media1.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${restaurant._id}/en`);
+
+			});
+			target.append(row);
+		}
+
+		const allRows = document.getElementsByTagName("tr");
+
+	} catch (error) {
+		const p = document.createElement("p");
+		p.innerText = `Error loading restaurants`;
+		target.append(p);
+		console.log(error.message);
+	}
+}
+
+async function getMenu(restaurant, url) {
+	try {
+		const response = await fetch (url);
+		if (!response.ok) throw new Error("invalid");
+		const json = await response.json();
+		const menu = json.courses;
+		
+		// dialog
+		const dialog = document.querySelector("dialog");
+		dialog.innerHTML = restaurantModal(restaurant, menu)
+		dialog.setAttribute("open", "")
+		
+	} catch (err) {
+		console.log(err.message);;
+	}
+}
+
+getRestaurants();
